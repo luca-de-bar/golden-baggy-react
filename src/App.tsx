9597eb460a51c5edd3ac6bcbba77e6a2a770df74
Luca <lucadebar33@gmail.com>
2024-08-10 11:51:50 +0200

    add : VideoPlayer.tsx component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import Navbar from './components/Navbar';
-import Carousel from './components/Carousel';
+{/* import Carousel from './components/Carousel'; */ }
+import VideoPlayer from './components/VideoPlayer';
 import InfoStrip from './components/InfoStrip';
 import { CarouselSize } from './components/CardCarousel';
 import MainItem from './components/MainItem';
@@ -9,7 +10,8 @@ const App = () => {
   return (
     <div>
       <Navbar />
-      <Carousel />
+      {/*<Carousel />*/ /* This one is optional, to use it you must first comment the VideoPlayer component and import. Choose from these two*/}
+      <VideoPlayer />
       <InfoStrip />
       <MainItem />
       <CarouselSize />
---
+++ src/components/VideoPlayer.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+
+const VideoPlayer: React.FC = () => {
+  return (
+    <div className="relative w-full">
+      <div className="relative h-64 sm:h-96 lg:h-128 overflow-hidden flex items-center justify-center">
+        <video
+          src="https://www.w3schools.com/html/mov_bbb.mp4" // URL of the video
+          className="w-full h-full object-cover lg:object-center-top"
+          autoPlay
+          loop
+          muted
+        />
+      </div>
+    </div>
+  );
+};
+
+export default VideoPlayer;
